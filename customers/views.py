8bd69f7661d041b537ddce6fb82f824cec66c100
bren0r <nuriomonov1@mail.ru>
2023-09-25 18:25:28 +0300

    first view for customers

--- customers/views.py
+++ customers/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Customer
 
-# Create your views here.
+
+def customer_list(request):
+    customers = Customer.objects.all()
+    return render(request, 'customers/customer_list.html', {'customers': customers})
+
+def customer_detail(request, customer_id):
+    customer = Customer.objects.get(id=customer_id)
+    return render(request, 'customers/customer_detail.html', {'customer': customer})
