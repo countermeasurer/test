d83af21939f3bfad0eb51a4a2d2d5d42306c289b
bren0r <nuriomonov1@mail.ru>
2023-09-25 21:41:27 +0300

    fixed mistake

--- customers/views.py
+++ customers/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
+from django.views import View
 from .models import Customer
 
+class CustomerView(View):
+    def customer_list(request):
+        customers = Customer.objects.all()
+        return render(request, 'customers/customer_list.html', {'customers': customers})
 
-def customer_list(request):
-    customers = Customer.objects.all()
-    return render(request, 'customers/customer_list.html', {'customers': customers})
+    def customer_detail(request, customer_id):
+        customer = Customer.objects.get(id=customer_id)
+        return render(request, 'customers/customer_detail.html', {'customer': customer})
 
-def customer_detail(request, customer_id):
-    customer = Customer.objects.get(id=customer_id)
-    return render(request, 'customers/customer_detail.html', {'customer': customer})
